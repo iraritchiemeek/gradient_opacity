5ae5d652ad5e659466d2db36dca342058eee67f9
iraritchiemeek <iraritchiemeek@gmail.com>
2015-07-23 11:17:34 +0800

    Add onclick to cycle new objects

--- controller.js
+++ controller.js
@@ -2,14 +2,17 @@ $(function(){
 	
 	var rectangle = new Rectangle	
 
-	for (var i = 0; i < 2; i++) {
-		rectangle.appendRectangle()
-	};
 
-	for (var i = $(".rectangle").length - 1; i >= 0; i--) {
-		rectangle.scale($(".rectangle")[i])
-		rectangle.position($(".rectangle")[i])
-		rectangle.applyGradient($(".rectangle")[i])
-	};
-		// rectangle.fillGradient()
+
+	$("#container").on('click', function(){
+		$(".rectangle").remove()
+		for (var i = 0; i < 2; i++) {
+			rectangle.appendRectangle()
+		};
+		for (var i = $(".rectangle").length - 1; i >= 0; i--) {
+			rectangle.scale($(".rectangle")[i])
+			rectangle.position($(".rectangle")[i])
+			rectangle.applyGradient($(".rectangle")[i])
+		};
+	})
 })
